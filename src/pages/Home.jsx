4b0f490bec03530a4c5b5b1c40115255c980a0cb
ricardo sotolongo rosabal <ricardosr@estudiantes.uci.cu>
2024-06-06 15:58:02 -0400

    First commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenido a la página de inicio</h1>
+      <Button variant="contained">Haz clic en mí</Button>
+    </div>
+  );
+}
+
+export default Home;
